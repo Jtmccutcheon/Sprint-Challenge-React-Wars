8cbbe8d2426c3d90a4855148d0de0cd0a6c00bd9
jtmccutcheon <jt.mccutcheon@live.com>
2019-08-16 13:38:35 -0400

    mvp

--- src/components/StarWarsPeeps.js
+++ src/components/StarWarsPeeps.js
@@ -3,46 +3,44 @@ import styled from "styled-components";
 
 function StarWarsPeeps(props) {
     // console.log(props);
-    const StyledDiv = styled.div`
-        /* display: flex; */
-        /* flex-direction: column; */
-        /* justify-content: flex-start; */
-        /* background-color: red; */
 
-        div {
-            border: 2px solid blue;
+
+
+    const StyledDiv = styled.div`
+        width: 28%;
+        margin-top: 5rem;
+        background-image: url('https://images.unsplash.com/photo-1533613220915-609f661a6fe1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80');
+        background-position: center;
+        /* position:fixed; */
+        border-radius: .6rem;
+        box-shadow: 1px 1px 5px #fff;
 
             h1 {
-                color: #443e3e;
+                color: #B4BDCC;
                 text-shadow: 1px 1px 5px #fff;
+                text-align: center;
             }
             p {
                 /* font-size: 5rem; */
                 text-transform: uppercase;
+                text-align:left;
+                color: white;
+                margin: 0 0 1rem 1rem;
+                font-family: Star Jedi, arial;
+
             }
-        }
     `;
 
-
-
     return (
-        <div>
-            <StyledDiv>
-                <div>
-                    <h1>{props.name}</h1>
-                    <div>
-                        <p>Gener: {props.gender}</p>
-                        <p>Hair Color: {props.haircolor}</p>
-                        <p>Eye Color:{props.eyecolor}</p>
-                        <p>Height (in): {props.height}</p>
-                        <p>Weight (mass): {props.mass}</p>
-                    </div>
-                </div>
 
-
-
-            </StyledDiv>
-        </div>
+        <StyledDiv>
+            <h1>{props.name}</h1>
+            <p>Gender: {props.gender}</p>
+            <p>Hair Color: {props.haircolor}</p>
+            <p>Eye Color:{props.eyecolor}</p>
+            <p>Height (in): {props.height}</p>
+            <p>Weight (mass): {props.mass}</p>
+        </StyledDiv>
     );
 }
 
