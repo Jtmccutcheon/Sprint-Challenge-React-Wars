061793094d47085e5aa80d626ad3dcd70c1fbc63
jtmccutcheon <jt.mccutcheon@live.com>
2019-08-16 11:54:14 -0400

    component working

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import ReactDOM from "react-dom";
+import axios from "axios";
+import StarWarsPeeps from "./components/StarWarsPeeps";
+
+import './App.css';
+
+const App = () => {
+  // Try to think through what state you'll need for this app before starting. Then build out
+  // the state properties here.
+  const [data, setData] = useState([]);
+
+  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
+  // side effect in a component, you want to think about which state and/or props it should
+  // sync up with, if any.
+  useEffect(() => {
+    axios.get(`https://swapi.co/api/people`).then(response => {
+      console.log(response.data);
+      const Peeps = response.data.results;
+      setData(Peeps);
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      <h1 className="Header">React Wars</h1>
+      <div>
+        {data.map(props => {
+          return <StarWarsPeeps limit={10}
+            name={props.name}
+            haircolor={props.hair_color}
+          />
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/StarWarsPeeps.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+function StarWarsPeeps(props) {
+    // console.log(props);
+
+    return (
+        <div>
+            <div>
+                <h1>{props.name}</h1>
+                <p></p>
+            </div>
+        </div>
+    );
+}
+
+export default StarWarsPeeps;
